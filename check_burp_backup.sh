01963efac8f3f8cceda3ad5c7cc6c423f37b7d44
Roland Hopferwieser <rhopfer@ica.jku.at>
2016-11-07 12:02:24 +0100

    Read directory from burp-server.conf

--- check_burp_backup.sh
+++ check_burp_backup.sh
@@ -3,7 +3,8 @@
 #                   Version 2, December 2004
 # 
 #Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
-# 
+#              2016 Roland Hopferwieser
+#
 #Everyone is permitted to copy and distribute verbatim or modified
 #copies of this license document, and changing it is allowed as long
 #as the name is changed.
@@ -20,6 +21,8 @@
 # * Statistics gathering and state of backup depending of BURP version (use backup_stats if it exists, and fall back to log.gz)
 # * Choose number of error for CRITICAL level
 
+VERSION=1.2
+BURP_SERVER_CONF=/etc/burp/burp-server.conf
 
 ## Variables
 #set -xv
@@ -27,23 +30,29 @@ STATE_OK=0
 STATE_WARNING=1
 STATE_CRITICAL=2
 STATE_UNKNOWN=3
-PERFDATA=""
-PERFDATAOPT=0
+PERFDATA=
+WARNING=36
+CRITICAL=72
 
-function usage()
-{
-	echo check_burp_backup.sh 1.1
+function usage() {
+	local dir=$(burpDir)
+	echo "$(basename $0) $VERSION"
 	echo "This plugin read the backup_stats file (or log.gz for burp < 1.4.x) and check the age and warning of the backup."
-	echo "Usage : -H <hostname> -d <directory> [-p] -w <minutes> -c <minutes>"
-	echo "Options :"
+	echo "Usage: -H <hostname> [-d <dir>] [-p] [-w <hours>] [-c <hours>]"
+	echo
+	echo "Options:"
 	echo "-H Name of backuped host (see clientconfdir)"
-	echo "-w WARNING number of minutes since last save "
-	echo "-w CRITICAL number of minutes since last save"
-	echo "-p Enable perfdata"
+	echo "-d Override burp directory ($dir)"
+	echo "-w WARNING number of minutes since last save ($WARNING)"
+	echo "-w CRITICAL number of minutes since last save ($CRITICAL)"
 }
 
-function convertToSecond()
-{
+unknown() {
+	echo "UNKNOWN : $1"
+	exit $STATE_UNKNOWN
+}
+
+function convertToSecond() {
 	local S=$1
 	((h=S/3600))
 	((m=S%3600/60))
@@ -51,16 +60,20 @@ function convertToSecond()
 	printf "%dh%dm%ds\n" $h $m $s
 }
 
+burpDir() {
+	if [[ ! -r "$BURP_SERVER_CONF" ]]; then
+		unknown "Can't read '$BURP_SERVER_CONF'!"
+	fi
+	local dir=$(awk '/^\s*directory/ { print $3; }' "$BURP_SERVER_CONF")
+	if [[ -z "$dir" || ! -r "$dir" ]]; then
+		unknown "Invalid burp directory '$dir'!"
+	fi
+	echo "$dir"
+}
 
 ## Start
-
-
-# Arguments check
-if [ "$#" = "0" ]
-then 
-	usage
-	exit $STATE_UNKNOWN
-fi
+DIR=
+HOST=
 
 # Manage arguments
 while getopts hH:d:pw:c: OPT; do
@@ -81,9 +94,6 @@ while getopts hH:d:pw:c: OPT; do
 		c)
 			CRITICAL=$OPTARG
 			;;
-		p)
-			PERFDATAOPT=1
-			;;
 		*)
 			usage
  			exit $STATE_UNKNOWN
@@ -91,60 +101,40 @@ while getopts hH:d:pw:c: OPT; do
 	esac
 done
 
-
-if [ $WARNING -gt $CRITICAL ]
-then
-	echo "UNKNOWN : Warning level is greater than Critical level"
-	exit $STATE_UNKNOWN
-else
-	WARNING=$(($WARNING * 60))
-	CRITICAL=$(($CRITICAL * 60))
-
+if [[ -z "$DIR" ]]; then
+	DIR=$(burpDir)
 fi
+DIR="$DIR/$HOST"
+LOG="$DIR/current/log.gz"
 
+[[ -n "$HOST" ]] || unknown "Empty host"
+[[ $CRITICAL -gt $WARNING ]] || unknown "Warning level is greater than critical level"
+[[ -r "$LOG" ]] || unknown "Can't read file '$LOG'"
 
-
-LOG=$DIR/$HOST/current/log.gz
-
-# Open laste BURP backup log file
-if [ ! -e $LOG ]
-then
-	echo "CRITICAL : $FILE doesn't exist!"
-	exit $STATE_CRITICAL
-fi
+WARNING=$(( WARNING * 3600 ))
+CRITICAL=$(( CRITICAL * 3600 ))
 
 # Unzip log file before read it. Mayabe you have zgrep
-TMP=/tmp/$HOST-check_burp.tmp
-zcat $LOG > $TMP
+TMP=$(mktemp /tmp/check_burp_backup-XXXXXX)
+zcat "$LOG" > $TMP
 
 # Statistics gathering
-WARNINGS=$(grep Warnings $TMP | awk '{print $NF}')
-NEW=$(grep "Grand total" $TMP | awk '{print $3}')
-CHANGED=$(grep "Grand total" $TMP | awk '{print $4}')
-UNCHANGED=$(grep "Grand total" $TMP | awk '{print $5}')
-DELETED=$(grep "Grand total" $TMP | awk '{print $6}')
-TOTAL=$(grep "Grand total" $TMP | awk '{print $7}')
+WARNINGS=$(awk '/Warnings/ {print $NF}' $TMP)
+read NEW CHANGED UNCHANGED DELETED TOTAL <<<$( awk '/Grand total:/ { print $3 " " $4 " " $5 " " $6 " " $7; }' $TMP)
+TIME=$( awk '/End time:/ { print $3 " " $4; }' $TMP)
 
-# date gathering
-DATE=$(grep "End time: " $TMP | awk '{print $3}')
-HEURE=$(grep "End time: " $TMP | awk '{print $4}')
-
-ENDSTAMP=$(date -d "$DATE $HEURE" +%s)
+ENDSTAMP=$(date -d "$TIME" +%s)
 NOW=$(date +%s)
 
 LAST=$(($NOW-$ENDSTAMP))
 LASTDIFF=$(convertToSecond $LAST)
 
-if [ $PERFDATAOPT -eq 1 ]
-then
-	PERFDATA=$(echo "| warnings=$WARNINGS; new=$NEW; changed=$CHANGED; unchanged=$UNCHANGED; deleted=$DELETED; total=$TOTAL")
-fi
+PERFDATA=$(echo "| warnings=$WARNINGS; new=$NEW; changed=$CHANGED; unchanged=$UNCHANGED; deleted=$DELETED; total=$TOTAL")
 
 # Clean tempory file
 rm $TMP
 
-if [ $LAST -gt $CRITICAL ] || [ $WARNINGS -gt 0 ]
-then
+if [ $LAST -gt $CRITICAL ] || [ $WARNINGS -gt 0 ]; then
 	echo "CRITICAL : Last backup $LASTDIFF ago with $WARNINGS errors $PERFDATA"
 	exit $STATE_CRITICAL
 else
